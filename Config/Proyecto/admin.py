563e4d7bc297904c00b84e1fbf7c9cbe13042eab
Diogenes <diogenespuig@gmail.com>
2020-11-04 20:13:39 -0300

    Se agregaron las tablas al admin site  y se ingreso el archivo superuserinfo.txt que tiene el usuario y contraseña paraingresar como admin al sitio

--- Config/Proyecto/admin.py
+++ Config/Proyecto/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from Proyecto.models import *
 
 # Register your models here.
+admin.site.register(Persona,)
+admin.site.register(Cliente,)
+admin.site.register(Administrador,)
+admin.site.register(Direccion,)
+admin.site.register(Estado,)
+admin.site.register(TipoHabitacion,)
+admin.site.register(Habitacion,)
