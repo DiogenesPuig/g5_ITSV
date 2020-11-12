6ace1ac6fed2f823288329dedfe62bab5255246a
FrancoRossiV <francoagustin433@gmail.com>
2020-11-12 16:29:59 -0300

    Agregando configuraciones al admin.py y arreglando algunos problemas con las imagenes & urls

--- Config/Config/urls.py
+++ Config/Config/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+from Config import settings
 
 urlpatterns = [
     path('jet', include('jet.urls','jet')),
@@ -23,3 +25,7 @@ urlpatterns = [
 
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- Config/Proyecto/admin.py
+++ Config/Proyecto/admin.py
@@ -1,11 +1,48 @@
 from django.contrib import admin
 from Proyecto.models import *
 
+
+
+
+
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'apellido','mail', 'telefono', 'codigo')
+
+    
+    fieldsets = (
+        ('Datos Personales', {
+            'fields' : ('nombre', 'apellido', 'codigo')
+        }),
+        ('Contacto', {
+            'fields' : ('telefono', 'mail')
+        })
+    )
+
+class AdministradorAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'apellido','mail', 'telefono', 'codigo')
+
+    
+    fieldsets = (
+        ('Datos Personales', {
+            'fields' : ('nombre', 'apellido', 'codigo')
+        }),
+        ('Contacto', {
+            'fields' : ('telefono', 'mail')
+        })
+    )
+    
+class EstadoAdmin(admin.ModelAdmin):
+    list_display = ('cliente', 'estado')    
+
+class HabitacionAdmin(admin.ModelAdmin):
+    list_display = ('precioNoche', 'numHabitacion', 'cantDormitorios', 'cantBanios', 'estado', 'tipohabitacion', 'img_habitacion')
+
+
 # Register your models here.
 admin.site.register(Persona,)
-admin.site.register(Cliente,)
-admin.site.register(Administrador,)
+admin.site.register(Cliente, ClienteAdmin)
+admin.site.register(Administrador, AdministradorAdmin)
 admin.site.register(Direccion,)
-admin.site.register(Estado,)
+admin.site.register(Estado, EstadoAdmin)
 admin.site.register(TipoHabitacion,)
-admin.site.register(Habitacion,)
+admin.site.register(Habitacion, HabitacionAdmin)
