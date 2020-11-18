856adf45b4c7cfdda8603d0dbacd09778d4d2689
Diogenes <diogenespuig@gmail.com>
2020-11-18 17:35:02 -0300

    Intentando usar ORM

--- Config/Proyecto/views.py
+++ Config/Proyecto/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.views.generic import TemplateView
+from Proyecto.models import Hotel
+
 # Create your views here.
 class HomeView(TemplateView):
     template_name = 'Proyecto/home.html'
+
+
+def hotel(request):
+    hotel= Hotel.objects.get(id=1)
+    return render(request, 'Proyecto/home.html',{'name': request.user, 'hotel': hotel.nombre,'estrellas':hotel.estrellas,'direccion':hotel.direccion})
