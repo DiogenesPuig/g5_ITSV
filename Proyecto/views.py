34aa1dcfcd460aabed6abb2693b8ba6e2c2e38f6
Diogenes <diogenespuig@gmail.com>
2020-11-19 16:17:55 -0300

    Se agrego un script.js y style.css (aun sin funcionalidad)

--- Proyecto/views.py
+++ Proyecto/views.py
@@ -1,26 +1,29 @@
 from django.shortcuts import render, render_to_response
 from django.views.generic import TemplateView
 from Proyecto.models import *
+import json
+
 
 # Create your views here.
 class HomeView(TemplateView):
     template_name = 'Proyecto/home.html'
 
-
-
     def get(self, request, *args, **kwargs):
         context = self.get_context_data(**kwargs)
-        #Creando publicaciones
-        h = Hotel.objects.all()
+        # Creando publicaciones
+        h = Hotel.objects.all().values_list()
+        jso = []
         hoteles = ""
         for hs in h:
-            hoteles += "<div> <h2>" +str(hs.nombre)+"</h2><br>" + "</div><br>"
-            print(hoteles)
-        context['hotel'] = hoteles
+            jso += {hs}
+        cadena = json.dumps(jso)
+        print(cadena)
+        context['hotel'] = cadena
         return self.render_to_response(context)
 
 
-
 def hotel(request):
-    hotel= Hotel.objects.get(id=1)
-    return render(request, 'Proyecto/home.html',{'name': request.user, 'hotel': hotel.nombre,'estrellas':hotel.estrellas,'direccion':hotel.direccion})
+    hotel = Hotel.objects.get(id=1)
+    return render(request, 'Proyecto/home.html',
+                  {'name': request.user, 'hotel': hotel.nombre, 'estrellas': hotel.estrellas,
+                   'direccion': hotel.direccion})
---
+++ templates/Proyecto/script.js
@@ -0,0 +1,3 @@
+data_from_django
+
+
