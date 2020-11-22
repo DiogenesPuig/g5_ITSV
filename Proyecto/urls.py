668ccf554014a34820854c78927cf7561cc96573
DiogenesPuig <diogenespuig@gmail.com>
2020-11-22 15:30:42 -0300

    Commit se sube hoteles.html incompleto

--- Proyecto/urls.py
+++ Proyecto/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('login', LoginView, name='login'),
     path('register', RegisterView,name='register'),
     path('logout',LogoutUser,name="logout"),
-    path('hoteles/<str:hotel>',HotelesView,name="hoteles")
+    path('hoteles/<str:Hotel>',HotelesView,name="hoteles"),
+    path('habitacion/str:Habitacion',HabitacionView,name='habitaciones'),
+
 ]
